2909c0209f3937de9a5221a74ba8808ab7e157be
Piryth <Arnaud.Endignous@etu.univ-grenoble-alpes.fr>
2022-02-11 18:08:42 +0100

    ajout

--- physics.js
+++ physics.js
@@ -28,44 +28,56 @@ var ground = Bodies.rectangle(0, window.innerHeight, screen.width*2, 16, {
 var circle = Bodies.circle(window.innerWidth/2, window.innerHeight/2, window.innerWidth/10, {
     isStatic: true,
     render: {
-        fillStyle: "rgba(0,0,0,.5)"
+        fillStyle: "rgba(0,0,0,0)"
     }
 });
 
-var cover = Bodies.rectangle(window.innerWidth/2-40, -300, 100, 100, {
+var cover = Bodies.rectangle(window.innerWidth/2-40, -300, 71, 71, {
     render: {
         sprite: {
             texture: "img/coruscant_ep.jpg",
-            xScale: 1/5,
-            yScale: 1/5
+            xScale: 1/7,
+            yScale: 1/7
         }
     }
 });
 
-var cover2 = Bodies.rectangle(window.innerWidth/2+40, -200, 100, 100, {
+var cover2 = Bodies.rectangle(window.innerWidth/2+40, -200, 71, 71, {
     render: {
         sprite: {
             texture: "img/N7.jpg",
-            xScale: 1/5,
-            yScale: 1/5
+            xScale: 1/7,
+            yScale: 1/7
         }
     }
 });
 
-var cover3 = Bodies.rectangle(window.innerWidth/4, -500, 100, 100, {
+var cover3 = Bodies.rectangle(window.innerWidth/4, -500, 71, 71, {
     render: {
         sprite: {
             texture: "img/great_hunt_ep.png",
-            xScale: 1/5,
-            yScale: 1/5
+            xScale: 1/7,
+            yScale: 1/7
+        }
+    }
+});
+
+
+var cover4 = Bodies.rectangle(window.innerWidth/4-window.innerWidth/8, -100, 71, 71, {
+    render: {
+        sprite: {
+            texture: "img/indomitable.png",
+            xScale: 1/7,
+            yScale: 1/7
         }
     }
 });
 
 Body.rotate(cover3, 1);
+Body.rotate(cover4, Math.PI/6);
 
 
-Composite.add(engine.world, [ground,cover,cover2,cover3,circle]);
+Composite.add(engine.world, [ground,cover,cover2,cover3,cover4,circle]);
 
 Render.run(render);
 
